6d60545726ebf07d5a897cbeb9038d337b9eb769
Matheus Luís <matheus.bernardi@lnls.br>
2024-01-07 12:42:03 -0300

    add blog content; formatting mdx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 import { Navbar } from "./components/nav";
 import { cn } from "./lib/utils";
@@ -18,14 +19,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={cn(
+        'text-black bg-white dark:text-white dark:bg-[#111010]',
+        GeistSans.variable,
+        GeistMono.variable
+      )}
+    >
       <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
-        <main
-          className={cn(
-            GeistSans.className,
-            "flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0"
-          )}
-        >
+        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
           {children}
         </main>
