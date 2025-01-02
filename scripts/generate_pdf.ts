425ba9ef73a83acd3e74889620fb8bc12b8baa73
matheus <matheuslbernardi@gmail.com>
2025-01-02 15:17:58 -0300

    add pdf options
    margin and header template with date

--- scripts/generate_pdf.ts
+++ scripts/generate_pdf.ts
@@ -6,9 +6,32 @@ const src = path.join(dir, "../public/resume.md");
 const output = path.join(dir, "../public/MatheusBernardi_resume.pdf");
 
 async function generatePDF() {
-  const pdf = await mdToPdf({ path: src }, { dest: output }).catch(
-    console.error,
-  );
+  const pdf = await mdToPdf(
+    { path: src },
+    {
+      dest: output,
+      pdf_options: {
+        format: "A4",
+        margin: {
+          top: "12mm",
+          bottom: "12mm",
+          right: "20mm",
+          left: "20mm",
+        },
+        headerTemplate: `<style>
+      section {
+        margin: 0 auto;
+        font-family: system-ui;
+        font-size: 11px;
+      }
+    </style>
+    <section>
+      <span class="title"></span>
+      <span>${new Date().toDateString()}</span>
+    </section>`,
+      },
+    },
+  ).catch(console.error);
 
   if (pdf) {
     Bun.write(pdf.filename, pdf.content);
