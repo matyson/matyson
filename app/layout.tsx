c206bbf53142b3e4e0c2179406cae3ea660b599a
matheus <matheuslbernardi@gmail.com>
2025-01-03 20:40:04 -0300

    update layout
    include paragraph spacing, auto margin, add view transitions and theme switch

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,10 @@ import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { ViewTransitions } from "next-view-transitions";
 import { Footer } from "./components/footer";
 import { Navbar } from "./components/nav";
+import ThemeSwitch from "./components/theme-switch";
 import { cn } from "./lib/utils";
 
 export const metadata: Metadata = {
@@ -21,20 +23,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html
-      suppressHydrationWarning
-      lang="en"
-      className={cn(GeistSans.variable, GeistMono.variable)}
-    >
-      <body className="mx-4 mb-40 mt-8 flex max-w-2xl flex-col bg-slate-50 text-black antialiased dark:bg-[#111010] dark:text-white md:flex-row lg:mx-auto">
-        <main className="mt-6 flex min-w-0 flex-auto flex-col px-2 md:px-0">
-          <ThemeProvider attribute="class">
-            <Navbar />
-            {children}
-            <Footer />
-          </ThemeProvider>
-        </main>
-      </body>
-    </html>
+    <ViewTransitions>
+      <html
+        suppressHydrationWarning
+        lang="en"
+        className={cn(GeistSans.variable, GeistMono.variable)}
+      >
+        <body className="tracking-tight antialiased">
+          <div className="flex min-h-screen flex-col justify-between p-8 pt-0 md:pt-8">
+            <ThemeProvider attribute="class">
+              <main className="mx-auto w-full max-w-[60ch] space-y-6">
+                <Navbar />
+                {children}
+              </main>
+              <Footer />
+              <div className="fixed right-4 top-4 z-40">
+                <ThemeSwitch />
+              </div>
+            </ThemeProvider>
+          </div>
+        </body>
+      </html>
+    </ViewTransitions>
   );
 }
