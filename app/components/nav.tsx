ff29d2914aba2c094e922ad2a51727a905c4b193
matheus <matheuslbernardi@gmail.com>
2025-01-03 20:39:08 -0300

    use transitions in navbar

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Link } from "next-view-transitions";
 import { cn } from "../lib/utils";
 
 const navItems = {
